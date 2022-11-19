8ffdb6fa87ae5ad64bcad007aac170cdfe72543f
touchthesky80 <touchthesky80@gmail.com>
2022-11-19 10:26:41 +0900

    Change web source with three.js

--- app/src/main/java/com/example/webviewandroidapp/MainActivity.java
+++ app/src/main/java/com/example/webviewandroidapp/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
@@ -29,13 +32,37 @@ public class MainActivity extends AppCompatActivity {
             @Override
             @RequiresApi(21)
             public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
-                return assetLoader.shouldInterceptRequest(request.getUrl());
+                try {
+                    WebResourceResponse response = assetLoader.shouldInterceptRequest(request.getUrl());
+
+                    if(response != null) {
+                        Log.d("JustPlane", "status code for  " + request.getUrl() + " is " + response.getStatusCode());
+                    } else {
+                        Log.d("JustPlane", "status code for  " + request.getUrl() + " is null");
+                    }
+
+                    return response;
+                }
+                catch(Exception e) {
+                    Log.e("JustPlane", e.getMessage());
+                    return null;
+                }
             }
 
             @Override
             @SuppressWarnings("deprecation") // for API < 21
             public WebResourceResponse shouldInterceptRequest(WebView view, String url) {
+                Log.d("JustPlane", url);
                 return assetLoader.shouldInterceptRequest(Uri.parse(url));
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.d("JustPlane", consoleMessage.message() + " -- From line " +
+                        consoleMessage.lineNumber() + " of " + consoleMessage.sourceId());
+                return true;
             }
         });
 
@@ -45,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setAllowUniversalAccessFromFileURLs(true);
         webSettings.setAllowFileAccess(true);
         webSettings.setAllowContentAccess(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webSettings.setMixedContentMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
 
         webView.loadUrl("https://appassets.androidplatform.net/assets/index.html");
     }
